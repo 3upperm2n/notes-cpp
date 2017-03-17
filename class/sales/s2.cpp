1cbe63adca908dd0382fc2e5833aa4b1d66481b1
Leiming Yu <leimingyu830@gmail.com>
2017-03-17 07:00:48 -0400

    fix read

--- class/sales/s2.cpp
+++ class/sales/s2.cpp
@@ -40,6 +40,10 @@ private:
 	double revenue = 0.0;
 }
 
+// 
+Sales_data add(const Sales_data&, const Sales_data&);
+ostream &print(ostream&, const Sales_data&);
+istream &read(istream&, Sales_data&);
 
 //-------------------------//
 // class member func
@@ -57,7 +61,7 @@ Sales_data &combine(const Sales_data& myData) {
 //-------------------------//
 istream &read(istream &is, Sales_data &item) {
 	double price = 0;
-	is >> item.bookNo << item.units_sold >> price;
+	is << item.bookNo << item.units_sold >> price;
 	item.revenue = item.units_sold * price;
 	return is;
 }
@@ -95,37 +99,6 @@ int main()
 		cerr << "No data ?!" << endl;	
 	}
 
-	/*
-	Sales_data data1, data2;
-
-	double price = 0;
-
-	// input 1
-	cin >> data1.bookNo >> data1.units_sold >> price;
-	data1.revenue = data1.units_sold * price;
-
-	// input 2
-	cin >> data2.bookNo >> data2.units_sold >> price;
-	data2.revenue = data2.units_sold * price;
-
-	if(data1.bookNo == data2.bookNo) {
-		unsigned totalCnt = data1.units_sold + data2.units_sold;
-		double totalRevenue = data1.revenue + data2.revenue;
-
-		cout << data1.bookNo << " " << totalCnt << " " << totalRevenue << " ";
-	
-		if( totalCnt != 0) {
-			cout <<  totalRevenue / totalCnt <<  endl;
-		} else {
-			cout <<	"(no sales)" << endl;
-		}
-
-		return 0;
-	} else {
-		cerr << "Data must refer to the same ISBN" << endl;
-		return -1;
-	}
-	*/
 
 	return 0;
 }
