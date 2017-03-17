7d1147c66434cfe74e4a2c2ae500185e82c5efa7
Leiming Yu <leimingyu830@gmail.com>
2017-03-16 21:05:11 -0400

    avg price

--- class/sales/s1.cpp
+++ class/sales/s1.cpp
@@ -22,6 +22,17 @@ ostream &print(ostream&, const Sales_data&);
 istream &read(istream&, Sales_data&);
 
 
+double Sales_data::avg_price () const
+{
+	if(units_sold) {
+		return revenue / units_sold;	
+	} else {
+		return 0;	
+	}
+}
+
+
+
 int main()
 {
 	Sales_data total;
