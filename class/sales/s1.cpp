4c6082e99ba5910c3fbbcff181c8111ea69694c7
Leiming Yu <leimingyu830@gmail.com>
2017-03-16 21:00:28 -0400

    add interface

---
+++ class/sales/s1.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Sales_data {
+	string isbn() { return bookNo; }
+	double avg_price() const; 
+	Sales_data &combine(const Sales_data&);
+
+	//-----------------
+	string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+};
+
+
+Sales_data add(const Sales_data&, const Sales_data&);
+ostream &print(ostream&, const Sales_data&);
+istream &read(istream&, Sales_data&);
+
+
+int main()
+{
+	Sales_data total;
+
+	if(read(cin, total)) { 								// read input 1
+		Sales_data trans;			// read input 2
+		while(read(cin, trans)) {
+			if(total.isbn() == trans.isbn()) {
+				total.combine(trans);	// isbn same, update the total
+			} else {
+				print(cout, total) << endl;	 // print input 1
+				total = trans;	// handle the next input
+			}
+		
+		}
+		print(cout, total) << endl; // print the last input
+	
+	} else {
+		cerr << "No data ?!" << endl;	
+	}
+
+	/*
+	Sales_data data1, data2;
+
+	double price = 0;
+
+	// input 1
+	cin >> data1.bookNo >> data1.units_sold >> price;
+	data1.revenue = data1.units_sold * price;
+
+	// input 2
+	cin >> data2.bookNo >> data2.units_sold >> price;
+	data2.revenue = data2.units_sold * price;
+
+	if(data1.bookNo == data2.bookNo) {
+		unsigned totalCnt = data1.units_sold + data2.units_sold;
+		double totalRevenue = data1.revenue + data2.revenue;
+
+		cout << data1.bookNo << " " << totalCnt << " " << totalRevenue << " ";
+	
+		if( totalCnt != 0) {
+			cout <<  totalRevenue / totalCnt <<  endl;
+		} else {
+			cout <<	"(no sales)" << endl;
+		}
+
+		return 0;
+	} else {
+		cerr << "Data must refer to the same ISBN" << endl;
+		return -1;
+	}
+	*/
+
+	return 0;
+}
