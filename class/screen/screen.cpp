946363dc4b41ec14198510a0ce124d8311c27108
Leiming Yu <leimingyu830@gmail.com>
2017-03-17 22:20:40 -0400

    add func

--- class/screen/screen.cpp
+++ class/screen/screen.cpp
@@ -6,17 +6,40 @@
 using namespace std;
 
 // size_type need to go with containers
+// it is like size_t, but more dynamic and tolerable on the data size
 
 class Screen
 {
 public:
 	typedef string::size_type pos;
+	
+	Screen() = default;
+	Screen(pos ht, pos wd, char c) : height(ht), width(wd), contents(ht*wd, c) {}
+
+	char get() const { return contents[cursor];	}	// implicit inline
+	inline char get(pos ht, pos wd) const;			// explicit inline
+	Screen &move(pos r, pos c);
+
 private:
 	pos cursor = 0;
 	pos height = 0;
 	pos width = 0;
 	string contents;
 };
+
+char Screen::get(pos ht, pos wd) const
+{
+	pos row = ht * width;
+	return contents[row + wd];
+}
+
+inline
+Screen& Screen::move(pos r, pos c) 
+{
+	pos row = r * width;
+	cursor = row + c;
+	return *this;
+}
 
 int main()
 {
